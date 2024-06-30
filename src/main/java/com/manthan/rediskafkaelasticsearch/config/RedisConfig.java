441bd10a3eeac0622b27a3dc6c3d2dc0219350aa
Manthan Patidar <manthanpatidar@gmail.com>
2024-06-30 22:00:59 +0530

    testing aws redis

--- src/main/java/com/manthan/rediskafkaelasticsearch/config/RedisConfig.java
+++ src/main/java/com/manthan/rediskafkaelasticsearch/config/RedisConfig.java
@@ -1,17 +1,17 @@
 package com.manthan.rediskafkaelasticsearch.config;
 
-//import com.manthan.rediskafkaelasticsearch.models.Post;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//
-//@Configuration
-//public class RedisConfig {
-//    @Bean
-//    RedisTemplate<Long, Post> redisTemplate(RedisConnectionFactory redisConnectionFactory){
-//        RedisTemplate<Long, Post> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setConnectionFactory(redisConnectionFactory);
-//        return redisTemplate;
-//    }
-//}
+import com.manthan.rediskafkaelasticsearch.models.Post;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    RedisTemplate<Long, Post> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<Long, Post> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+}
--- src/main/java/com/manthan/rediskafkaelasticsearch/services/PostService.java
+++ src/main/java/com/manthan/rediskafkaelasticsearch/services/PostService.java
@@ -5,7 +5,7 @@ import com.manthan.rediskafkaelasticsearch.exceptions.PostDoesNotExistException;
 import com.manthan.rediskafkaelasticsearch.models.Post;
 import com.manthan.rediskafkaelasticsearch.repositories.PostRepository;
 import org.springframework.cache.annotation.Cacheable;
-//import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,14 +15,12 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class PostService {
     private PostRepository postRepository;
-//    private RedisTemplate<Long, Post> redisTemplate;
+    private RedisTemplate<Long, Post> redisTemplate;
 
-    public PostService(PostRepository postRepository
-//                       RedisTemplate<Long, Post> redisTemplate
-    )
+    public PostService(PostRepository postRepository, RedisTemplate<Long, Post> redisTemplate)
     {
         this.postRepository = postRepository;
-//        this.redisTemplate = redisTemplate;
+        this.redisTemplate = redisTemplate;
     }
 
     public List<Post> getAllPosts(){
@@ -33,16 +31,16 @@ public class PostService {
     // so disabling this.redisTemplate
 //    https://premika-17.medium.com/implementing-redis-in-spring-boot-3d2756e5ab69
 //    @Cacheable(value = "post")
-    public Post getPost(Long postId) throws AuthorDoesNotExistException, PostDoesNotExistException, InterruptedException {
+    public Post getPost(Long postId) throws PostDoesNotExistException, InterruptedException {
         Post post = null;
-//        post = this.redisTemplate.opsForValue().get(postId);
-//        if(post != null) return post;
+        post = this.redisTemplate.opsForValue().get(postId);
+        if(post != null) return post;
 
         Thread.sleep(5 * 1000);
         Optional<Post> postOptional = this.postRepository.findById(postId);
         if(postOptional.isEmpty()) throw new PostDoesNotExistException(postId);
         post = postOptional.get();
-//        redisTemplate.opsForValue().set(postId, post, 20, TimeUnit.SECONDS);
+        redisTemplate.opsForValue().set(postId, post, 20, TimeUnit.SECONDS);
         return post;
     }
 
